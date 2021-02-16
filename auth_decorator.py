d493aca6c9745574908f919d9f6683bee299921a
balaji2122 <79143801+balaji2122@users.noreply.github.com>
2021-02-16 17:09:44 +0530

    Create auth_decorator.py

---
+++ auth_decorator.py
@@ -0,0 +1,20 @@
+def ok_user_and_password(username, password):
+    return username == app.config['USERNAME'] and password == app.config['PASSWORD']
+
+def authenticate():
+    message = {'message': "Authenticate."}
+    resp = jsonify(message)
+
+    resp.status_code = 401
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Main"'
+
+    return resp
+
+def requires_authorization(f):
+    @functools.wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not ok_user_and_password(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
